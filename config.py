cd6f7c8407d68f9213eb911f2ecffed8572086bc
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-06 17:10:05 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# 환경변수 모으기
+
+import os
+
+# 노션에 연결중인 현재가 DB의 토큰값.
+NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+
+# 통합 거래내역 DB
+NOTION_TRADE_DB_ID = os.environ["NOTION_TRADE_DB_ID"]
+
+# 순수익 DB 링크
+NOTION_NET_PROFIT_DB_ID = os.environ["NOTION_NET_PROFIT_DB_ID"]
