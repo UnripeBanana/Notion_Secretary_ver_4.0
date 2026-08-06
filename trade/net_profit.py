a54cc6a123d8deba7e1677bc1a9a25d598488966
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-06 17:15:52 +0900

    Create net_profit.py

---
+++ trade/net_profit.py
@@ -0,0 +1,21 @@
+from config.notion import NOTION_NET_PROFIT_DB_ID
+from notion.client import notion
+from notion.rich_text import rich_text
+from utils.day_log import today_and_time_is
+
+def net_profit(prop, profit):
+
+    trade_profit = profit if prop == "trade" else 0
+    dividend_profit = profit if prop == "dividend" else 0
+    
+    notion.pages.create(
+        parent={
+            "database_id": NOTION_NET_PROFIT_DB_ID
+        },
+        
+        properties={
+            "이익": {"number": trade_profit},
+            "배당이익": {"number": dividend_profit},
+            "마지막 업데이트": rich_text(today_and_time_is())
+        }
+    )
