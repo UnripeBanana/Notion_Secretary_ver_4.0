c52ab6ae8dd5d117ae163dd36c68bcef9485ed4d
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-07 15:15:48 +0900

    Update updator.py

--- notion/trade/updator.py
+++ notion/trade/updator.py
@@ -1,5 +1,6 @@
 from notion.client import notion 
 from notion.trade.net_profit import net_profit
+from utils.day_log import today_and_time_is
 
 def trade_updator(results):
     for id, raw_prop in results.items():
